ef8f577633ec36b813be2403dd02ff7ed83d0055
gkaragoz <gkaragoz96@gmail.com>
2019-06-29 00:04:59 +0300

    Keyboard buttons change text color onPressed.

---
+++ Worduel/Assets/Scripts/UI/KeyboardTextColors.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class KeyboardTextColors : MonoBehaviour {
+
+    [SerializeField]
+    private Color _pressedColor = Color.white;
+    [SerializeField]
+    private Color _defaultColor = Color.white;
+
+    private TextMeshProUGUI _text;
+
+    private void Awake() {
+        _text = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void SetPressedColor() {
+        _text.color = _pressedColor;
+    }
+
+    public void SetDefaultColor() {
+        _text.color = _defaultColor;
+    }
+
+}
